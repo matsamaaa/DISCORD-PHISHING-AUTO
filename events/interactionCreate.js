ad04e57c596438f2044f2e732b9a25618d206ee9
mat_sama <mateosejourne@gmail.com>
2024-03-03 03:27:12 +0100

    owner filter update

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,5 +1,6 @@
 const { check, error } = require("../utils/console");
 const log = console.log;
+const { IDS } = process.env;
 
 module.exports = {
     name: 'interactionCreate',
@@ -11,6 +12,11 @@ module.exports = {
 
         if (interaction.isChatInputCommand()) {
             try {
+                const owners = IDS.toString().trim().split(',');
+                if(!owners.includes(member.id)) {
+                    log(error + "User can't use this command")
+                    return interaction.reply({ content: `❌ » You can't used this command !`, ephemeral: true });
+                } 
                 const command = await client.slashCommands.get(commandName);
                 command.execute(interaction, client)
                 log(check + `command ${commandName} execute by ${member.user.username}`)
